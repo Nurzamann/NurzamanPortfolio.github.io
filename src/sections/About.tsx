6928de5e8bef871bf03f4e4ad513ecb5c547daf7
Nurzamann <nurzaman0001@gmail.com>
2025-05-15 02:10:26 +0700

    file about.jsx on progress

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -1,3 +1,89 @@
+import { SectionHeader } from "@/components/SectionHeader";
+import { Card } from "@/components/Card";
+import StarIcon from "@/assets/icons/star.svg";
+import bookImage from "@/assets/images/book-cover.png";
+import Image from "next/image";
+import JavascriptIcon from "@/assets/icons/square-js.svg";
+import HTMLIcon from "@/assets/icons/html5.svg";
+import CssIcon from "@/assets/icons/css3.svg";
+import ReactIcon from "@/assets/icons/react.svg";
+import ChromeIcon from "@/assets/icons/chrome.svg";
+import GithubIcon from "@/assets/icons/github.svg";
+import { TechIcon } from "@/components/TechIcon";
+import mapImage from "@/assets/images/map.png";
+import smileMemoji from "@/assets/images/memoji-smile.png";
+
+const ToolboxItems = [
+  {
+    title: "Javascript",
+    iconType: JavascriptIcon,
+  },
+  {
+    title: "HTML5",
+    iconType: HTMLIcon,
+  },
+  {
+    title: "CSS3",
+    iconType: CssIcon ,
+  },
+  {
+    title: "React",
+    iconType: ReactIcon,
+  },
+  {
+    title: "Chrome",
+    iconType: ChromeIcon,
+  },
+  {
+    title: "Github",
+    iconType: GithubIcon,
+  },
+]
+
 export const AboutSection = () => {
-  return <div>About Section</div>;
+  return (
+    <div className="pb-96">
+      <SectionHeader
+        eyebrow="About Me"
+        title="A Glimpse Into My World"
+        description="Learn more about whi i am, what i do, and what inspires me."
+      />
+      <div>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Reads</h3>
+            <p>Explore the books shaping my perspectives.</p>
+          </div>
+          <Image src={bookImage} alt="Book cover" />
+        </Card>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>My Toolbox</h3>
+            <p>Explore the technologies and tools i use to craft exceptional digital experiences.</p>
+          </div>
+          <div>
+            {ToolboxItems.map((item) => (
+              <div key={item.title}>
+                <TechIcon component={item.iconType}/>
+                <span>{item.title}</span>
+              </div>
+            ))}
+          </div>
+        </Card>
+        <Card>
+          <div>
+            <StarIcon />
+            <h3>Beyond the Code</h3>
+            <p>Explore my interests and hobbies beyond the digital realm.</p>
+          </div>
+        </Card>
+        <Card>
+            <Image src={mapImage} alt="map" />
+            <Image src={smileMemoji} alt="smiling memoji" />
+        </Card>
+      </div>
+    </div>
+  );
 };
